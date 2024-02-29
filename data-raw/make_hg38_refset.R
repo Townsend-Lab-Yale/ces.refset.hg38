8ebecb606e7a6a7a0d4c2ae321f491257c25e9f4
Jeff Mandell <jeff@mandells.org>
2024-02-29 13:00:25 -0500

    Now separate RefCDS and gr_genes for use in annotations and dNdScv; added transcript table with more info

--- data-raw/make_hg38_refset.R
+++ data-raw/make_hg38_refset.R
@@ -1,98 +1,103 @@
 library(data.table)
-library(rtracklayer)
-library(cancereffectsizeR)
+library(cancereffectsizeR) # v2.9.0
+library(GenomicRanges)
 
-# Require cancereffectsizeR >= 2.4.0 for latest version of refset due to fixes in create_refset/build_RefCDS
-stopifnot(packageVersion('cancereffectsizeR') >= as.package_version('2.4.0'))
+# Fun fact: rtracklayer's GTF parser doesn't handle tags correctly.
+gtf_file = 'untracked/gencode.v45.basic.annotation.gtf.gz' # local Gencode GTF
+gtf = as.data.table(rtracklayer::import(gtf_file))
 
-# External data source:
-# Gencode GRCh38 "basic" GTF, release 38
+# Read in file again to get tags          
+gtf$tag = NULL
+tags = fread(gtf_file, skip = 'chr') # first data line starts with a chromosome name
 
-# Loading Gencode GTF, release 38 (latest release as of 09/15/21)
-gen = as.data.table(import("~/reference/gencode/gencode.v38.basic.annotation.gtf.gz"))
+# 9th column (V9) has tag entries (along with other stuff)
+stopifnot(identical(tags$V4, gtf$start)) # Verify records are in order 
 
-# Subset to protein-coding since we're building CDS annotations
-# In the future, would be nice to include additional annotations somewhere
-gen = gen[gene_type == 'protein_coding']
-gen[, support := transcript_support_level]
-gen[is.na(support) | support == "NA", support := '6'] # yeah, both "NA" and NAs
-gen[, support := as.numeric(support)]
+gtf[, tag := tags$V9]
 
-
-# For each gene, prioritize CCDS (consensus CDS) with highest transcript support, and then
-# for genes with no such transcripts, accept the most-supported non-CCDS transcripts. When
-# there are multiple transcripts tied for most supported, keep them all.
-just_ccds = gen[type == 'CDS' & ! is.na(ccdsid), .SD[support == min(support)], by = "gene_id"]
-missing_genes = setdiff(gen$gene_id, just_ccds$gene_id)
-from_missing = gen[missing_genes, on = 'gene_id'][type == 'CDS', .SD[support == min(support)], by = "gene_id"]
-best_by_gene = rbind(just_ccds, from_missing)
-
-# remove the handful of chrM records
-best_by_gene = best_by_gene[seqnames != 'chrM']
-
-sum(width(reduce(makeGRangesFromDataFrame(best_by_gene)))) # 33.1 Mb (similar to old RefCDS)
-
-## A few transcripts will be tossed by build_RefCDS, but most will be good
-# uniqueN(best_by_gene$gene_id) # 19,942
-# uniqueN(best_by_gene$transcript_id) # 30,641
+gtf[, support := transcript_support_level]
+gtf[is.na(support) | support == "NA", support := '6'] # yeah, both "NA" and NAs
+gtf[, support := as.numeric(support)]
+gtf[, exon_number := as.numeric(exon_number)]
+gtf[, seqnames := gsub('^chr', '', as.character(seqnames))]
+gtf = gtf[seqnames %in% c(1:22, 'X', 'Y')]
 
 
-# A handful of well-studied genes like TP53 have lots of transcripts. For simplicity,
-# we'll find transcripts that do not have any exons not already incorporated in longer transcripts,
-# and we'll filter these out.
-transcripts_by_gene = split(best_by_gene, by = 'gene_id')
-best_by_gene[, exon_number := as.numeric(exon_number)]
-exons_per_transcript = best_by_gene[, .(num_exons = max(exon_number)), by = 'transcript_id']
+gtf[, is_mane := tag %like% 'tag \\"MANE_Select']
+gtf[, is_mane_plus := tag %like% 'tag \\"MANE_Plus_Clinical']
+# gtf[, is_basic := tag %like% 'tag \\"basic\\"'] # all records are basic since we're now using the basic GTF
+gtf[, is_seleno := tag %like% 'tag \\"seleno']
+gtf[, is_non_ATG_start := tag %like% 'tag \\"non_ATG_start']
+gtf[, is_NMD_exception := tag %like% 'tag \\"NMD_exception']
 
-best_by_gene[exons_per_transcript, num_exons := num_exons, on = 'transcript_id']
-best_by_gene = best_by_gene[order(gene_id, -num_exons)]
+# Capture various splice annotations (all "non_canonical" tags relate to splicing)
+gtf[, noncanonical_splice := tag %like% 'tag \"non_canonical']
+gtf[, NAGNAG_splice_site := tag %like% 'tag \"NAGNAG_splice_site']
 
-find_novel_transcripts = function(gene_dt) {
-  used_exons = character()
-  unneeded_transcripts = character()
-  gene_dt = setDT(gene_dt[, .(transcript_id, exon_id)], key = 'transcript_id')
-  setkey(gene_dt, 'transcript_id')
-  unique_transcripts = unique(gene_dt$transcript_id)
-  for (transcript in unique_transcripts) {
-    current_exons = gene_dt[transcript, exon_id]
-    new_exons = setdiff(current_exons, used_exons)
-    if(length(new_exons) == 0) {
-      unneeded_transcripts = c(unneeded_transcripts, transcript)
-    }
-    used_exons = c(used_exons, new_exons)
-  }
-  return(setdiff(unique_transcripts, unneeded_transcripts))
-}
+cds = gtf[transcript_type == 'protein_coding' & type == 'CDS']
 
-# Takes a minute to run
-useful_transcripts_by_gene = best_by_gene[, .(transcript_id = find_novel_transcripts(.SD)), by = 'gene_id']
+uniqueN(cds$transcript_id) # 63,727
+sum(width(reduce(makeGRangesFromDataFrame(cds)))) # 34,760,548
 
-best_by_gene = best_by_gene[useful_transcripts_by_gene$transcript_id, on = 'transcript_id']
-# Same number of genes (19,942), but now down to 29,475 transcripts.
-# uniqueN(best_by_gene$gene_id)
-# uniqueN(best_by_gene$transcript_id)
-
+cds[, .(uniqueN(gene_id), uniqueN(transcript_id))] # 19,681 genes with 63,727 transcripts
 
 # chr17:7675994 (TP53) is not at an "essential splice site" as defined by Martincorena et al.,
 # but mutations there have experimentally confirmed effects on splicing.
 # We will grab all TP53 transcripts that include this site and manually specify the splice status.
 # (It turns out that for all such transcripts, 7675994 is at a CDS start position.)
-## sort(best_by_gene[gene_name == "TP53"][, abs(7675994 - as.numeric(end))])[1:10]
-## sort(best_by_gene[gene_name == "TP53"][, abs(7675994 - as.numeric(start))])[1:15]
-custom_splice_list = best_by_gene[gene_name == "TP53" & start == 7675994, .(protein_id, start)]
+## sort(cds[gene_name == "TP53"][, abs(7675994 - as.numeric(end))])[1:10]
+## sort(cds[gene_name == "TP53"][, abs(7675994 - as.numeric(start))])[1:15]
+custom_splice_list = cds[gene_name == "TP53" & start == 7675994, .(protein_id, start)]
 custom_splice_list = setNames(as.list(custom_splice_list$start), custom_splice_list$protein_id)
 
-refcds = build_RefCDS(gtf = best_by_gene, genome = 'hg38', use_all_transcripts = TRUE, 
-                      additional_essential_splice_pos = custom_splice_list)
+
+refcds_anno = build_RefCDS(gtf = cds, genome = 'hg38', use_all_transcripts = TRUE, 
+                           additional_essential_splice_pos = custom_splice_list, cores = 4)
+
+# There will be a warning about some of the protein IDs in the custom_splice_list not being used; that's okay.
+refcds_dndscv = build_RefCDS(gtf = cds, genome = 'hg38', use_all_transcripts = FALSE, 
+                             additional_essential_splice_pos = custom_splice_list, cores = 4)
 
 # For a default exome, we'll take all exon regions of protein-coding genes (with no transcript support filtering)
-# This gives 80 Mb due to UTRs, as opposed to 34.7 Mb if just CDS regions were used.
-exome = reduce(makeGRangesFromDataFrame(gen[type == 'exon' & seqnames != 'chrM']))
+# This gives 81 Mb due to UTRs, as opposed to ~35 Mb if just CDS regions were used.
+exome = reduce(makeGRangesFromDataFrame(gtf[type == 'exon' & transcript_type == 'protein_coding']))
+
+# We will store some information about coding and noncoding transcripts
+transcript_info = gtf[(transcript_type == 'protein_coding' & type %in% c('UTR', 'transcript', 'CDS')) |
+                        (transcript_type != 'protein_coding' & type == 'transcript'), 
+                      .(chr = seqnames, start, end, strand, type, gene_id, gene_type, gene_name, transcript_id,
+                        transcript_type, exon_number = as.integer(exon_number), protein_id, is_mane, is_mane_plus, non_ATG_start = is_non_ATG_start,
+                        NMD_exception = is_NMD_exception, seleno = is_seleno, noncanonical_splice, NAGNAG_splice_site)]
+
+# Verify these columns are transcript-level, then combine into one field
+transcript_level_cols = c('non_ATG_start', 'NMD_exception', 'noncanonical_splice', 'NAGNAG_splice_site', 'seleno')
+for(col in transcript_level_cols) {
+  stopifnot(uniqueN(transcript_info[, .SD, .SDcols = c('transcript_id', col)]) == uniqueN(transcript_info$transcript_id))
+}
+
+transcript_melted = unique(melt(transcript_info, id.vars = 'transcript_id', measure.vars = transcript_level_cols)[value == TRUE])
+transcript_melted[, variable := paste0('<', variable, '>')]
+transcript_melted[, transcript_tags := paste(variable, collapse = ''), by = 'transcript_id']
+to_merge = unique(transcript_melted[, .(transcript_id, transcript_tags)])
+
+transcript_info[to_merge, transcript_tags := transcript_tags, on = 'transcript_id']
+transcript_info = transcript_info[, .SD, .SDcols = setdiff(names(transcript_info), transcript_level_cols)]
+
+# Note that makeGRangesFromDataFrame(transcript_info[transcript_type == 'protein_coding' & type %in% c('UTR', 'CDS')])
+# reproduces the default exome.
 
 dir.create('tmp_ref') # will move data into refset package inst/refset directory
 
-create_refset(output_dir = "tmp_ref/", refcds_output = refcds, species_name = 'human',
-              genome_build_name = 'hg38', BSgenome_name = 'hg38', supported_chr = c(1:22, 'X', 'Y'), 
-              default_exome = exome, exome_interval_padding = 0)
+create_refset(output_dir = "tmp_ref/", refcds_anno = refcds_anno, refcds_dndscv = refcds_dndscv,
+              species_name = 'human', genome_build_name = 'hg38', BSgenome_name = 'hg38', 
+              supported_chr = c(1:22, 'X', 'Y'), default_exome = exome, exome_interval_padding = 0,
+              transcript_info = transcript_info)
 
-# Finish by copying into inst/refset
+
+
+
+
+
+
+
+
