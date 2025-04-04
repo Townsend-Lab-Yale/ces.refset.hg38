3fd6c0f683d5ce46cf50241ee9ab223ed8a0dd8c
Jeff Mandell <jeff@mandells.org>
2025-04-04 14:16:02 -0400

    Revert "Update cached chromInfo"
    This reverts commit b3e3ffcf6562b51aa466792fcf439e0810a526b3.

--- data-raw/get_cached_chromInfo.R
+++ data-raw/get_cached_chromInfo.R
@@ -9,11 +9,10 @@ bsg = BSgenome::getBSgenome(ces.refset.hg38::ces.refset.hg38$genome)
 ucsc_info = getFromNamespace(".UCSC_cached_chrom_info", "GenomeInfoDb")[["hg38"]]
 ucsc_info = GenomeInfoDb:::.add_ensembl_column(ucsc_info, "hg38")
 
-# Figure out name by inspecting output of names(getFromNamespace(".NCBI_cached_chrom_info", "GenomeInfoDb")) and choosing latest version
-curr_ncbi_name = "GCF_000001405.40"
-ncbi_info = getFromNamespace(".NCBI_cached_chrom_info", "GenomeInfoDb")[[curr_ncbi_name]]
+# Figure out name by inspecting output of getFromNamespace(".NCBI_cached_chrom_info", "GenomeInfoDb") and choosing latest version
+ncbi_info = getFromNamespace(".NCBI_cached_chrom_info", "GenomeInfoDb")[["GCF_000001405.40"]]
 
 cached_chromInfo = list()
 cached_chromInfo[['UCSC']] = list(name = "hg38", value = ucsc_info)
-cached_chromInfo[['NCBI']] = list(name = "GCF_000001405.40", value = ncbi_info)
+cached_chromInfo[['NCBI']] = list(name = "GCF_000001405.39", value = ncbi_info)
 saveRDS(cached_chromInfo, 'inst/refset/cached_chromInfo.rds')
